737993c67fa504bde7d90bbaf76b7eb3fbc861c6
kevwang424 <kev.wang424@gmail.com>
2017-01-24 10:47:28 -0500

    first draft

--- js/models/list.js
+++ js/models/list.js
@@ -1,3 +1,15 @@
 'use strict';
 // List Model
 
+class List{
+
+  constructor(title){
+    this.title = title
+    this.tasks = []
+    List.all.push(this)
+    this.id = List.all.length
+  }
+
+}
+
+List.all = []
